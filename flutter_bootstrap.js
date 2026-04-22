d55607ae4da9bd1bc32aee56a18c0aa9ad407812
wishrohitv <wishrohitv@users.noreply.github.com>
2026-04-22 11:25:11 +0000

    deploy: 6f53cdcdc1ffccffd9bd384d884e448b4f15a1c7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "874712501" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1338564278" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
